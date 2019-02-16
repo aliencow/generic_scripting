c4cf699aaa0b34949644d8688c41c05184e845ed
juan.nouche <juan.nouche@thegearing.com>
2019-02-16 20:52:01 +0100

    Actualización de contenidos

--- collapsible_layout.py
+++ collapsible_layout.py
@@ -1,20 +1,27 @@
 # -*- coding: utf-8 -*-
+# Author: Miguel Martinez Lopez
+#
+# Version: 0.4
+# Uncomment the next line to see my email
+# print("Author's email: %s"%"61706c69636163696f6e616d656469646140676d61696c2e636f6d".decode("hex"))
+
 
 try:
-    from Tkinter import *
+    from Tkinter import PhotoImage, Frame, Label, Widget
+    from Tkconstants import *
 except ImportError:
-    from tkinter import *
+    from tkinter import PhotoImage, Frame, Label, Widget
+    from tkinter.constants import *
 
-import os
 import base64
 
 class CollapsibleFrame(Frame):
-    def __init__(self, master, text=None, borderwidth=2, width=0, height=1, interior_padx=5, interior_pady=5, background=None, caption_separation=1, caption_font=None, caption_builder=None, icon_x=0):
+    def __init__(self, master, text=None, borderwidth=2, width=0, height=16, interior_padx=0, interior_pady=8, background=None, caption_separation=8, caption_font=None, caption_builder=None, icon_x=0):
         Frame.__init__(self, master)
         if background is None:
             background = self.cget("background")
 
-        self.configure(background='#5d5d5d', relief = SUNKEN)
+        self.configure(background='#5d5d5d')
 
         self._is_opened = False
 
@@ -37,7 +44,7 @@ class CollapsibleFrame(Frame):
         self._containerFrame = Frame(self, borderwidth=borderwidth, width=width*2, height=height, relief=None, background='#494949')
         self._containerFrame.pack(expand=True, fill=X, pady=(containerFrame_pady,0))
 
-        self.interior = Frame(self._containerFrame, background='#494949')
+        self.interior = Frame(self._containerFrame, background='#ff0000')
 
         self._collapseButton = Label(self, borderwidth=0, image=self._iconOpen, relief=RAISED, background='#5d5d5d')
         self._collapseButton.place(in_= self._containerFrame, x=icon_x, y=-(height_of_icon+1), anchor=N+W, bordermode="ignore")
@@ -102,108 +109,21 @@ class CollapsibleFrame(Frame):
         else:
             self.open()
 
+if __name__ == "__main__":
+    try:
+        from Tkinter import Tk, Button
+    except ImportError:
+        from tkinter import Tk, Button
 
-cursors = ["arrow","circle","clock","cross","dotbox","exchange","fleur",
-            "heart","man","mouse","pirate", "plus", "shuttle","sizing",
-            "spider","spraycan","star","target","tcross","trek", "watch"]
-# http://www.tcl.tk/man/tcl8.4/TkCmd/cursors.htm
+    root = Tk()
+    root.wm_geometry("400x300+0+0")
+    root.configure(bg='#444444')
 
-reliefs = [FLAT,RAISED,SUNKEN,GROOVE,RIDGE]
+    cf1 = CollapsibleFrame(root, text ="Esta es una prueba", interior_padx=5)
+    cf1.pack(fill=X)
 
-class Colors():
-    def __init__(self):
-        self.background = '#444444'
-        self.frameBG = '#494949'
-        self.label = '#bbbbbb'
-        self.buttonBG = '#5d5d5d'
-        self.buttonFG = '#eeeeee'
-        self.entryBG = '#2b2b2b'
+    for i in range(3):
+        Button(cf1.interior, text="button %s"%i).pack(side=LEFT)
 
-    @property
-    def background(self):
-        return self.background
-    @background.setter
-    def background(self, value):
-        self.background = value
-
-    @property
-    def frameBG(self):
-        return self.frameBG
-    @frameBG.setter
-    def frameBG(self, value):
-        self.frameBG = value
-
-    @property
-    def label(self):
-        return self.label
-    @label.setter
-    def label(self, value):
-        self.label = value
-
-    @property
-    def buttonBG(self):
-        return self.buttonBG
-    @buttonBG.setter
-    def buttonBG(self, value):
-        self.buttonBG = value
-
-    @property
-    def buttonFG(self):
-        return self.buttonFG
-    @buttonFG.setter
-    def buttonFG(self, value):
-        self.buttonFG = value
-
-    @property
-    def entryBG(self):
-        return self.entryBG
-    @entryBG.setter
-    def entryBG(self, value):
-        self.entryBG = value
-
-
-
-
-class Installer():
-
-    def resource_paths(self, relative_path):
-        """ Get absolute path to resource, works for dev and for PyInstaller """
-        try:
-            # PyInstaller creates a temp folder and stores path in _MEIPASS
-            base_path = sys._MEIPASS
-        except Exception:
-            base_path = os.path.abspath(".")
-
-        return os.path.join(base_path, relative_path)
-
-
-
-
-colors = Colors()
-inst = Installer()
-
-window = Tk()
-
-iconPath = inst.resource_paths('resources/conct_icon.ico')
-#root.iconbitmap('./resources/PB.ico')
-window.iconbitmap(iconPath)
-window.geometry('640x480')
-window.configure(bg=colors.background)
-
-window.title("Welcome to LikeGeeks app")
-
-label = Label(window, text="Hello papanatas\nque te pasa??", font=("Arial Bold", 12), justify = LEFT, fg=colors.label, bg=colors.background, borderwidth=2) #RIGHT #CENTER #LEFT
-#label.grid(column=0, row=0)
-label.pack(fill=X)
-
-button = Button(window, text="Click Me",font=("Arial Bold", 20), borderwidth=0, bg=colors.buttonBG, fg=colors.buttonFG, cursor='hand2', justify = CENTER)
-#button.grid(column=3, row=1)
-button.pack(fill=X, padx=2, pady=1)
-
-
-cf1 = CollapsibleFrame(window, text ="Esta es una prueba", interior_padx=5)
-cf1.pack(fill=X,padx=2, pady=1)
-for i in range(3):
-    Button(cf1.interior, text="button %s"%i).pack(fill=X, padx=5)
-
-window.mainloop()
+    cf1.update_width()
+    root.mainloop()

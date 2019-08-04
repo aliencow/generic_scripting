3cac9efd1860fc32a171caf877db4d58d244b168
juan.nouche <juan.nouche@thegearing.com>
2019-08-04 23:07:33 +0200

    changes in bat

--- config/actualizar.bat
+++ config/actualizar.bat
@@ -1,8 +1,10 @@
 ﻿@echo off
+
 REM Poner en el arranque de WINDOWS para tener actualizados todos los gits
 REM Queda guardado C:\Users\JUAN\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\actualizar.bat
 REM lo ejecuta al arranque de windows para buscar la carpeta de inicio de windows en el ejecutor de comandos
 REM Alt+R teclar shell:startup (usuario local) o shell:common startup (todos los usuarios)
+
 F:
 cd F:\proyectos_python\flask_pymongo
 git pull

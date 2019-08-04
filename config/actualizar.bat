1f4575adb3632b66f11dcf2a8809f5e19191d592
juan.nouche <juan.nouche@thegearing.com>
2019-08-04 22:45:57 +0200

    actualizar bat confirmed

--- config/actualizar.bat
+++ config/actualizar.bat
@@ -1,19 +1,18 @@
-@echo off
+﻿@echo off
 REM Poner en el arranque de WINDOWS para tener actualizados todos los gits
-REM Queda guardado en /Users/juan.nouche/desktop/actualizar.sh
-REM Para ejecutar source /Users/juan.nouche/desktop/actualizar.sh
-REM El comando esta puesto en ./bash_profile en el $HOME
-
+REM Queda guardado C:\Users\JUAN\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\actualizar.bat
+REM lo ejecuta al arranque de windows para buscar la carpeta de inicio de windows en el ejecutor de comandos
+REM Alt+R teclar shell:startup (usuario local) o shell:common startup (todos los usuarios)
 F:
-cd F:\ALMACEN1TB\proyectos_python\flask_pymongo
+cd F:\proyectos_python\flask_pymongo
 git pull
-cd F:\ALMACEN1TB\proyectos_python\generic_scripting
+cd F:\proyectos_python\generic_scripting
 git pull
-cd F:\ALMACEN1TB\proyectos_python\tablapythonweb
+cd F:\proyectos_python\tablapythonweb
 git pull
-cd F:\ALMACEN1TB\proyectos_python\tablesdir
+cd F:\proyectos_python\tablesdir
 git pull
-cd F:\ALMACEN1TB\proyectos_python\the_gearing\aom_playblast
+cd F:\proyectos_python\the_gearing\aom_playblast
 git pull
-cd F:\ALMACEN1TB\proyectos_python\the_gearing\concatenator
+cd F:\proyectos_python\the_gearing\concatenator
 git pull

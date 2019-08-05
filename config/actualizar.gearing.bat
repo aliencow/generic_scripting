6a116981e0bc258fcd40a9f1aed35241f048d642
Juan R Nouche <juan.nouche@gmail.com>
2019-08-05 18:55:21 +0200

    prueba con git directo

--- config/actualizar.gearing.bat
+++ config/actualizar.gearing.bat
@@ -5,16 +5,16 @@ REM Queda guardado C:\Users\JUAN\AppData\Roaming\Microsoft\Windows\Start Menu\Pr
 REM lo ejecuta al arranque de windows para buscar la carpeta de inicio de windows en el ejecutor de comandos
 REM Alt+R teclar shell:startup (usuario local) o shell:common startup (todos los usuarios)
 
-F:
-cd F:\proyectos_python\flask_pymongo
+C:
+cd C:\Users\juan.nouche\Documents\juan\git_projects\flask_pymongo
 git pull
-cd F:\proyectos_python\generic_scripting
+cd C:\Users\juan.nouche\Documents\juan\git_projects\generic_scripting
 git pull
-cd F:\proyectos_python\tablapythonweb
+cd C:\Users\juan.nouche\Documents\juan\git_projects\tablapythonweb
 git pull
-cd F:\proyectos_python\tablesdir
+cd C:\Users\juan.nouche\Documents\juan\git_projects\tablasdir
 git pull
-cd F:\proyectos_python\the_gearing\aom_playblast
+cd C:\Users\juan.nouche\Documents\juan\git_projects\the_gearing\aom_playblast
 git pull
 cd F:\proyectos_python\the_gearing\concatenator
 git pull
